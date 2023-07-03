de8413c2f56d3c073f85af12821f53310559abc9
kurulo kezemiakin <pipipupi.kom@gmail.com>
2023-07-03 11:12:15 +0300

    a

---
+++ main.py
@@ -0,0 +1,44 @@
+if __name__ == "__main__":
+    # Task 1
+    # num1 = input("Введіть перще число: ")
+    # num2 = input("Введіть друге число: ")
+    # num3 = input("Введіть третє число: ")
+    #
+    # fullNum = num1 + num2 + num3
+    # print(fullNum)
+
+    # Task 2
+    # first_Number = input("Введіть перше число: ")
+    # second_Number = input("Введіть друге число: ")
+    # third_Number = input("Введіть третє число: ")
+    # fourth_Number = input("Введіть четверте число: ")
+    #
+    # whole_Number = first_Number + second_Number + third_Number + fourth_Number
+    # print(whole_Number)
+    #
+    # print(int(first_Number) * int(second_Number) * int(third_Number) * int(fourth_Number))
+
+    # Task 3
+    # meters = int(input("Введіть метри: "))
+    # centimeters = meters * 100
+    # decimeters = meters * 10
+    # millimeters = meters * 1000
+    # miles = meters / 1609.344
+    #
+    # print("Метрів:", meters)
+    # print("Сантиметрів: ", centimeters)
+    # print("Дециметрів: ", decimeters)
+    # print("Міліметрів: ", millimeters)
+    # print("Миль: ", miles)
+
+    # Task 4
+    # base = int(input("Введіть розмір основи: "))
+    # height = int(input("Введіть розмір висоти: "))
+    # S = 0.5 * base * height
+    #
+    # print("Площа цього трикутника: ", S)
+
+    # Task 5
+    # custom_Numbers = str(input("Введіть числа: ")[::-1])
+    #
+    # print(custom_Numbers)
