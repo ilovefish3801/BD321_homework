da0033902d8c131275a05e994ba4187747904201
kurulo kezemiakin <pipipupi.kom@gmail.com>
2023-07-28 10:43:45 +0300

    a

--- main.py
+++ main.py
@@ -1,44 +1,43 @@
+def taskOne(list):
+    product = 1
+    for i in list:
+        product = product * i
+    return product
+
+
+def taskTwo(list):
+    return min(list)
+
+
+# 3 завдання не знаю чи можна зробити ви казали, що формули простих чисел немає але якщо рішення є скажіть його будь ласка
+
+def taskFour(list, delNumber):
+    try:
+        delnumIndex = list.index(delNumber)
+    except Exception as err:
+        return "Ви вибрали число якого немає в списку"
+
+    list.pop(delnumIndex)
+    return list
+
+
+def taskFive(listOne, listTwo):
+    mergedList = []
+    for i in listOne:
+        for a in listTwo:
+            if i == a:
+                mergedList.append(a)
+
+    return mergedList
+
+
+def taskSix(list, degree):
+    finishedList = []
+    for i in list:
+        i = i ** degree
+        finishedList.append(i)
+    return finishedList
+
+
 if __name__ == "__main__":
-    # Task 1
-    # num1 = input("Введіть перще число: ")
-    # num2 = input("Введіть друге число: ")
-    # num3 = input("Введіть третє число: ")
-    #
-    # fullNum = num1 + num2 + num3
-    # print(fullNum)
-
-    # Task 2
-    # first_Number = input("Введіть перше число: ")
-    # second_Number = input("Введіть друге число: ")
-    # third_Number = input("Введіть третє число: ")
-    # fourth_Number = input("Введіть четверте число: ")
-    #
-    # whole_Number = first_Number + second_Number + third_Number + fourth_Number
-    # print(whole_Number)
-    #
-    # print(int(first_Number) * int(second_Number) * int(third_Number) * int(fourth_Number))
-
-    # Task 3
-    # meters = int(input("Введіть метри: "))
-    # centimeters = meters * 100
-    # decimeters = meters * 10
-    # millimeters = meters * 1000
-    # miles = meters / 1609.344
-    #
-    # print("Метрів:", meters)
-    # print("Сантиметрів: ", centimeters)
-    # print("Дециметрів: ", decimeters)
-    # print("Міліметрів: ", millimeters)
-    # print("Миль: ", miles)
-
-    # Task 4
-    # base = int(input("Введіть розмір основи: "))
-    # height = int(input("Введіть розмір висоти: "))
-    # S = 0.5 * base * height
-    #
-    # print("Площа цього трикутника: ", S)
-
-    # Task 5
-    # custom_Numbers = str(input("Введіть числа: ")[::-1])
-    #
-    # print(custom_Numbers)
+    
