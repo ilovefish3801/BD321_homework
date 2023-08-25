1a2627b0a51ce20d279234f1c63e769f777338aa
kurulo kezemiakin <pipipupi.kom@gmail.com>
2023-08-25 10:01:17 +0300

    a

--- main.py
+++ main.py
@@ -1,44 +1,40 @@
+import random
+
 if __name__ == "__main__":
     # Task 1
-    # num1 = input("Введіть перще число: ")
-    # num2 = input("Введіть друге число: ")
-    # num3 = input("Введіть третє число: ")
+    # number = input("Введіть вираз: ")
     #
-    # fullNum = num1 + num2 + num3
-    # print(fullNum)
+    # if '+' in number:
+    #     result = number.split('+')
+    #     print(int(result[0]) + int(result[1]))
+    # elif '-' in number:
+    #     result = number.split('-')
+    #     print(int(result[0]) - int(result[1]))
+    # elif '*' in number:
+    #     result = number.split('*')
+    #     print(int(result[0]) * int(result[1]))
+    # else:
+    #     result = number.split('/')
+    #     print(int(result[0]) / int(result[1]))
 
     # Task 2
-    # first_Number = input("Введіть перше число: ")
-    # second_Number = input("Введіть друге число: ")
-    # third_Number = input("Введіть третє число: ")
-    # fourth_Number = input("Введіть четверте число: ")
-    #
-    # whole_Number = first_Number + second_Number + third_Number + fourth_Number
-    # print(whole_Number)
-    #
-    # print(int(first_Number) * int(second_Number) * int(third_Number) * int(fourth_Number))
+    arr = []
+    for i in range(9):
+        arr.append(random.randint(-100, 100))
 
-    # Task 3
-    # meters = int(input("Введіть метри: "))
-    # centimeters = meters * 100
-    # decimeters = meters * 10
-    # millimeters = meters * 1000
-    # miles = meters / 1609.344
-    #
-    # print("Метрів:", meters)
-    # print("Сантиметрів: ", centimeters)
-    # print("Дециметрів: ", decimeters)
-    # print("Міліметрів: ", millimeters)
-    # print("Миль: ", miles)
-
-    # Task 4
-    # base = int(input("Введіть розмір основи: "))
-    # height = int(input("Введіть розмір висоти: "))
-    # S = 0.5 * base * height
-    #
-    # print("Площа цього трикутника: ", S)
-
-    # Task 5
-    # custom_Numbers = str(input("Введіть числа: ")[::-1])
-    #
-    # print(custom_Numbers)
+    zeroCount = 0
+    negativeCount = 0
+    positiveCount = 0
+    for i in arr:
+        if i < 0:
+            negativeCount += 1
+        elif i > 0:
+            positiveCount += 1
+        else:
+            zeroCount += 1
+    print(f"Список заповненмй випадковими числами: {arr}\n")
+    print(f"Максимальний елемент у списку: {max(arr)}")
+    print(f"Мінімальний елемет у списку: {min(arr)}")
+    print(f"Кількість від'ємних чисел у списку: {negativeCount}")
+    print(f"Кількість додатніх чисел у списку: {positiveCount}")
+    print(f"Кількість нулів: {zeroCount}")
